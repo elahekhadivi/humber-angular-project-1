08e6bc50e3fbcbe3c43947577ae6b9f4de61fdf6
elahe_khavidi <elahe.khavidi@akerna.com>
2022-05-12 20:24:01 -0400

    app

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,34 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-export interface RawProductData
-{
-  id: number;
-  item_name: string;
-  item_description: string;
-  storage_location: string;
-  inventory_level: number;
-  image: string;
-}
-export interface ProductData{
-  item_name: string;
-  item_description: string;
-  image: string;
-}
-
-function ProductData(rawProductData: RawProductData): ProductData
-{
-  const ProductData ={
-    rawProductData.item_name,
-    
-    rawProductData.item_description,
-    rawProductData.image,
-  }
-  return ProductData;
-}
-
 @Component({
-  selector: 'cards-component',
-  templateUrl: './cards-component.component.html',
-  styleUrls: ['./cards-component.component.scss']
+  selector: 'card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
 })
 
 export class CardsComponentComponent implements OnInit {
